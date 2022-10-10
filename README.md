cda0586bd78bf4f5ca695bdc9b7ad091c332c2ec
peng00bo00 <bpeng45@gatech.edu>
2022-10-10 11:11:12 +0400

    update

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # GAMES301
-Course materials for GAMES301 Surface Parameterization.
+Course materials for [GAMES301 Surface Parameterization](http://staff.ustc.edu.cn/~renjiec/GAMES301/index.html).
---
+++ hws/matlab_template/test.m
@@ -0,0 +1,13 @@
+[x, t] = readObj('cathead');  % read triangle mesh from file
+
+B = findBoundary(x, t);   % find the boundary loop of the mesh
+
+uv = x(:, 2:3);   % trivial parameterization 
+distortion = x(:, 1);  % arbitrary distortion for visulization
+
+
+%% draw
+figure; subplot(121); h = drawmesh(t, x, B);
+subplot(122); h2 = drawmesh(t, uv);
+
+set(h2, 'CData', distortion, 'facecolor', 'interp');  % 
