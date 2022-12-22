83100f86839e9c2f4fe681899bd8ad1593a5963c
peng00bo00 <bpeng45@gatech.edu>
2022-12-22 21:34:52 +0400

    update

--- hws/hw2/DynamicDeformalbes/polar_decomposition_rv.m
+++
@@ -1,5 +0,0 @@
-function [R S] = polar_decomposition_rv(F)
-  [U Sigma V] = svd_rv(F);
-  R = U * V';
-  S = V * Sigma * V';
-end
--- hws/hw2/DynamicDeformalbes/vec.m
+++
@@ -1,4 +0,0 @@
-function [x] = vec(A)
-    [rows cols] = size(A);
-    x = reshape(A, rows * cols, 1);
-end
